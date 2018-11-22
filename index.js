3608bd6c942691bacd691b14bdd70347fbe6414f
Jacob Clark <jacob.clark@bbc.co.uk>
2018-11-22 21:23:57 +0000

    Initial commit

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 
 const app = express();
 const port = 3000;
+const webpackConfig = require('./webpack.config.dev')[0];
+const compiler = webpack(webpackConfig);
 
-export default (app) => {
-    const webpackConfig = require('./webpack.config.dev')[0];
-    const compiler = webpack(webpackConfig);
-
-    app.use(webpackDevMiddleware(compiler, {}));
-}
-
-
-if (process.env.NODE_ENV === "dev") developmentMode(app);
+app.use(webpackDevMiddleware(compiler, {}));
 
 app.get('/', (req, res) => res.send("Hello World"));
 
